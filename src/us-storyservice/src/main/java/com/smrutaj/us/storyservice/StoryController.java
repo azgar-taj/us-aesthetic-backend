72f70f60d37afb4c8084d9284729f466743e98ff
Abdul Azgar Taj <aat10@iibbs.ac.in>
2024-07-30 03:02:13 +0530

    CORS support

--- src/us-storyservice/src/main/java/com/smrutaj/us/storyservice/StoryController.java
+++ src/us-storyservice/src/main/java/com/smrutaj/us/storyservice/StoryController.java
@@ -5,7 +5,7 @@ import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin
 @RestController
 public class StoryController {
 
